201efa2719ebabf0ffc0fe6722552f01f103a3d6
Drake B <drakeboard719@riversideunified.org>
2024-01-22 14:15:35 -0800

    Finished Lesson 6

--- John Lemon/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/GameEnding.cs
+++ John Lemon/Assets/UnityTechnologies/3DBeginnerComplete/Scripts/GameEnding.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
@@ -8,7 +9,9 @@ public class GameEnding : MonoBehaviour
     public float displayImageDuration = 1f;
     public GameObject player;
     bool m_IsPlayerAtExit;
+    bool m_IsPlayerCaught;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
     float m_Timer;
     
     // Start is called before the first frame update
@@ -22,19 +25,31 @@ public class GameEnding : MonoBehaviour
     {
         if(m_IsPlayerAtExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
         }
-        
-        if(m_Timer > fadeDuration + displayImageDuration)
+
+        else if(m_IsPlayerCaught)
         {
-            Application.Quit();
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart)
     {
         m_Timer += Time.deltaTime;
         exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+        
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            if(doRestart)
+            {
+                SceneManager.LoadScene(0);
+            }
+            else
+            {
+                Application.Quit();
+            }
+        }    
     }
 
     void OnTriggerEnter(Collider other)
@@ -43,5 +58,10 @@ public class GameEnding : MonoBehaviour
         {
             m_IsPlayerAtExit = true;
         }
+    }
+    
+    public void CaughtPlayer ()
+    {
+        m_IsPlayerCaught = true;
     }
 }
